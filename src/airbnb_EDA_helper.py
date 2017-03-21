9c21b097950c6202e5c0f163290fe9e8e98fa414
John Kim <johnkim@Johns-MacBook-Pro.local>
2017-03-20 17:07:12 -0700

    add meanline to box compare plotter

--- src/airbnb_EDA_helper.py
+++ src/airbnb_EDA_helper.py
@@ -137,22 +137,27 @@ def plot_boxplot_compare(df, by, column1, column2, rot=0, fontsize=8, figsize=(1
 
     sorted_groups = df.groupby(by).mean()[column1].sort_values(ascending=False).index
 
-    whiskerprops1 = dict(linestyle='--', linewidth=0.5, color='black')
+    capprops1 = dict(linestyle='')
+    whiskerprops1 = dict(linestyle='')
     medianprops1 = dict(linestyle='--', linewidth=2., color='black')
+    meanprops1 = dict(linestyle='-', linewidth=2., color='black')
+    boxprops1 = dict(linestyle='')
     data1 = [df[df[by]==group][column1] for group in sorted_groups ]
-    box1 = ax.boxplot(data1, labels=sorted_groups, widths=0.4, medianprops=medianprops1);
+    box1 = ax.boxplot(data1, labels=sorted_groups, widths=0.4, showmeans=True, meanline=True,
+                      boxprops=boxprops1, meanprops=meanprops1, medianprops=medianprops1, capprops=capprops1, whiskerprops=whiskerprops1);
 
-    capprops2 = dict(linestyle='--', linewidth=1.0, color='red')
-    whiskerprops2 = dict(linestyle='--', linewidth=0.5, color='red')
-    boxprops2 = dict(linestyle='--', linewidth=0.5, color='red')
-    medianprops2 = dict(linestyle='--', linewidth=2., color='red')
+    capprops2 = dict(linestyle='--', linewidth=1.0, color='green')
+    whiskerprops2 = dict(linestyle='--', linewidth=0.5, color='green')
+    boxprops2 = dict(linestyle='--', linewidth=0.5, color='green')
+    meanprops2 = dict(linestyle='-', linewidth=2., color='green')
+    medianprops2 = dict(linestyle='--', linewidth=2., color='green')
     data2 = [df[df[by]==group][column2] for group in sorted_groups ]
-    box2 = ax.boxplot(data2, labels=sorted_groups, patch_artist=True, widths=0.4,
-                      boxprops=boxprops2, medianprops=medianprops2, capprops=capprops2, whiskerprops=whiskerprops2)
+    box2 = ax.boxplot(data2, labels=sorted_groups, patch_artist=True, widths=0.4, showmeans=True,meanline=True,
+                      boxprops=boxprops2, meanprops=meanprops2, medianprops=medianprops2, capprops=capprops2, whiskerprops=whiskerprops2)
 
     for box in box2['boxes']:
         box.set_alpha(0.3)
-        box.set_facecolor('red')
+        box.set_facecolor('green')
 
     for i,d in enumerate(sorted_groups):
         y = df[df[by]==d][column1]
@@ -162,7 +167,7 @@ def plot_boxplot_compare(df, by, column1, column2, rot=0, fontsize=8, figsize=(1
     for i,d in enumerate(sorted_groups):
         y = df[df[by]==d][column2]
         x = np.random.normal(i+1.1+jitter_offset, 0.02, len(y))
-        ax.plot(x, y, ms=3, marker="o", linestyle="None", alpha=0.5, c='red')
+        ax.plot(x, y, ms=3, marker="o", linestyle="None", alpha=0.5, c='green')
     #ax.boxplot()
 
     ax.set_ylabel(column1)
@@ -178,6 +183,11 @@ def plot_boxplot_compare(df, by, column1, column2, rot=0, fontsize=8, figsize=(1
     #     ax.set_ylim(0,upper_lim)
     plt.xticks(fontsize=fontsize)
     plt.yticks(fontsize=fontsize)
+
+    if not show_outliers:
+        iqr = df[column1].quantile(0.75) - df[column1].quantile(0.25)
+        upper_lim = df[column1].quantile(0.75)+3*iqr
+        ax.set_ylim(0,upper_lim)
 
     plt.show()
 
